d072c54df87d3cb688c57698732faf51b516ad24
Jan Baranek <janek@fsbs.pl>
2014-03-07 22:57:05 +0100

    signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    gid = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gid}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ TestApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # fast hashes for tests
+  ActiveModel::SecurePassword.min_cost = true
 end
